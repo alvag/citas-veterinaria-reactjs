485fe01314e5a196c4c028f8f14a67dc94e331c8
Max Alva <alva85@gmail.com>
2018-12-03 22:59:21 -0500

    Formulario para agregar citas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './css/App.css';
 import AgregarCita from './components/AgregarCita';
+import Header from './components/Header';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <AgregarCita />
+            <div className="container">
+                <Header title="Administrador de Pacientes de Veterinaria" />
+                <div className="row">
+                    <div className="col-md-6">
+                        <AgregarCita />
+                    </div>
+                </div>
             </div>
         );
     }
